3d67ecc96b8a1f186d22b4b7f847d465770a0e7b
Matt Hardman <matthardman@matts-mbp.home>
2017-11-28 15:49:49 +0000

    add post todos route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,29 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var {mongoose} = require('./db/mongoose.js');
+var {Todo} = require('./models/todo.js');
+var {User} = require('./models/user.js');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req,res) => {
+	console.log(req.body);
+	var newTodo = new Todo( {
+		text: req.body.text
+	})
+
+	newTodo.save().then((r) => {
+		console.log('saved', r)
+		res.send(r);
+	}, (e) => {
+		console.log('not saved', e)
+		res.status(400).send(e);
+	})
+});
+
+app.listen(3000, () => {
+	console.log('listening on port 3000');
+})
