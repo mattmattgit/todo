548401f38a3dc7e883317fd3a217ee43be5c2f9a
Matt Hardman <matthardman@matts-mbp.home>
2017-11-29 21:48:11 +0000

    todos with id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
@@ -31,6 +32,19 @@ app.get('/todos', (req,res) => {
 		res.status(400).send(e);
 	});
 });
+
+app.get('/todos/:id', (req,res) => {
+	let id = req.params.id
+	if(!ObjectID.isValid(id)){
+		res.status(400).send()
+	}
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+			res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((e) => res.status(400).send());
+})
 
 app.listen(3000, () => {
 	console.log('listening on port 3000');
